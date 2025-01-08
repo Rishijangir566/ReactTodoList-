df43430018adb5e71cbff99397ffd200e98ae753
Rishijangir566 <rishijangirjr@gmail.com>
2025-01-08 23:52:57 +0530

    edit function add

--- src/App.jsx
+++ src/App.jsx
@@ -2,74 +2,50 @@ import { useState } from 'react'
 
 
 function Todo() {
-  const [input, setInput] = useState(0)
+  const [input, setInput] = useState("")
   const [task,setTask] = useState([])
+   const [editingId , setEditingId]=useState(null)
 
     function addTask(){
-        const obj={id:Date.now(),task:input}
-        setTask([...task , obj])
-        setInput("")
+      if(editingId){
+          setTask(
+            task.map((oneTask)=> 
+              oneTask.id===editingId?{...task , taskToDo:input}:task
+            )
+          );
+          setEditingId(null)
+      }
+       else{
+         const obj={id:Date.now(),taskToDo:input}
+          setTask([...task , obj])
+          setInput("")
+       } 
     }
 
-    function Edit(obj){
-     
+    function Edit(id){
+       const taskToEdit = task.find((obj)=>obj.id===id)
+       setInput(taskToEdit.taskToDo)
+       setEditingId(id)
     }
+
     function Delete(id){
       setTask(task.filter(obj=>obj.id!=id))
     }
 
-    // function trial() {
-
-    //   if (isEditing) {
-  
-    //     setTask(
-    //       Tasks.map((obj) =>
-    //         obj.id === changedObjectId ? { ...obj, task: input } : obj
-    //       )
-    //     );
-  
-    //     setIsEditing(false);
-    //     setChangedObjectId(null);
-    //   } else {
-    //     addTask();
-    //   }
-    // }
-  
-    // function addTask() {
-    //   const Obj = { id: Date.now(), task: input, isChecked: false };
-    //   setTask([...Tasks, Obj]);
-    //   setInput("");
-    // }
-  
-    // function Delete(id) {
-    //   setTask(Tasks.filter((obj) => obj.id !== id));
-    // }
-  
-    // function Edit(obj) {
-    //   setInput(obj.task);
-    //   setChangedObjectId(obj.id);
-    //   setIsEditing(true);
-    //   // flag = true;
-    // }
-  
-    // function toggleCheckbox(id) {
-    //   setTask(Tasks.map((obj) =>
-    //     obj.id === id ? { ...obj, isChecked: !obj.isChecked } : obj
-    //   ));
-    // }
+   
   
   return (
     <>
       <input type="text" placeholder="Enter Your Task" value={input} 
       onChange={(e)=>setInput(e.target.value)} />
-      <button onClick={addTask}>Add</button>
+      <button onClick={addTask}>{editingId? "Edit Task":"Add Task"} </button>
 
       <ul>
         {task.map((obj)=>{
-          return  <li key={obj.id}>{obj.task}
+          return  <li key={obj.id}>{obj.taskToDo}
 
-           <button onClick={()=>Edit(obj)}>Edit</button>
            <button onClick={()=>Delete(obj.id)}>Delete</button>
+           <button onClick={()=>Edit(obj.id)}>Edit</button>
 
           </li>
         })}
